1f647e505161d3fff40c0cc0a7466570ced2fbff
dusha <dushasam26@gmail.com>
2024-10-30 18:40:14 +0100

    add parameters by @RequesParam annotation

--- src/main/java/com/samoilenko/springCourse/config/controllers/FirstController.java
+++ src/main/java/com/samoilenko/springCourse/config/controllers/FirstController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,9 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname){
 
         System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
