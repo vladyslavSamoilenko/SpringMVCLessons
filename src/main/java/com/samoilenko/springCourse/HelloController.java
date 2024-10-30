2d696d2a1df32ba4efbd22012885c3886a80a801
dusha <dushasam26@gmail.com>
2024-10-30 02:14:20 +0100

    deleted xml configuration files

--- src/main/java/com/samoilenko/springCourse/HelloController.java
+++ src/main/java/com/samoilenko/springCourse/HelloController.java
@@ -1,2 +1,13 @@
-package com.samoilenko.springCourse;public class HelloController {
+package com.samoilenko.springCourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/helloWorld")
+    public String sayHello(){
+        return "hello_world";
+    }
 }
--- src/main/java/com/samoilenko/springCourse/config/MySpringMVCDispatcherServletInitializer.java
+++ src/main/java/com/samoilenko/springCourse/config/MySpringMVCDispatcherServletInitializer.java
@@ -1,2 +1,21 @@
-package com.samoilenko.springCourse.config;public class MySpringMVCDispatcherServletInitializer {
+package com.samoilenko.springCourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
