b5c2916eb9a3f8fcfb44f1e3887fb2b42258b80b
dusha <dushasam26@gmail.com>
2024-10-30 18:32:14 +0100

    add parameters by HTTPServletRequest

--- src/main/java/com/samoilenko/springCourse/config/controllers/FirstController.java
+++ src/main/java/com/samoilenko/springCourse/config/controllers/FirstController.java
@@ -1,15 +1,22 @@
 package com.samoilenko.springCourse.config.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
